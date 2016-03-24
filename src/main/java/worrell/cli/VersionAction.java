e05be38cc8ec82369c23899c2990bb47afd6d713
Michael Atkisson <mpatkisson@gmail.com>
2016-03-24 05:56:00 -0400

    Resolves #5 - Corrects bad version print in CLI
    Fixes an issue in which the CLI "version" command printed the
    snapshot version, not the release version.

--- src/main/java/worrell/cli/VersionAction.java
+++ src/main/java/worrell/cli/VersionAction.java
@@ -12,7 +12,15 @@ public class VersionAction extends Action {
 
     @Override
     public void run(String[] args) {
+<<<<<<< HEAD
         System.out.println("0.1.0-SNAPSHOT");
+=======
+<<<<<<< HEAD
+        System.out.println("0.2.0-SNAPSHOT");
+=======
+        System.out.println("0.1.0");
+>>>>>>> ac752f5... Resolves #5 - Corrects bad version print in CLI
+>>>>>>> e1b221d... Resolves #5 - Corrects bad version print in CLI
     }
 
 }
