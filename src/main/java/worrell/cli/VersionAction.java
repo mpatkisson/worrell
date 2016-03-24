ecfd7fcc6597cd653d9af7bffd1ab2c0dd32cca8
Michael Atkisson <mpatkisson@gmail.com>
2016-03-24 05:49:47 -0400

    Increases the version number for the next release

--- src/main/java/worrell/cli/VersionAction.java
+++ src/main/java/worrell/cli/VersionAction.java
@@ -13,6 +13,7 @@ public class VersionAction extends Action {
     @Override
     public void run(String[] args) {
 <<<<<<< HEAD
+<<<<<<< HEAD
         System.out.println("0.1.0-SNAPSHOT");
 =======
 <<<<<<< HEAD
@@ -21,6 +22,9 @@ public class VersionAction extends Action {
         System.out.println("0.1.0");
 >>>>>>> ac752f5... Resolves #5 - Corrects bad version print in CLI
 >>>>>>> e1b221d... Resolves #5 - Corrects bad version print in CLI
+=======
+        System.out.println("0.2.0-SNAPSHOT");
+>>>>>>> 6c2595f... Increases the version number for the next release
     }
 
 }
